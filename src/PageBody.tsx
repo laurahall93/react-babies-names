03f7fae59b441178d3dc3679534120368d613d10
Laura Hall <podinjshmi1993@gmail.com>
2023-07-14 14:25:43 +0100

    added exercise3

--- src/PageBody.tsx
+++ src/PageBody.tsx
@@ -1,49 +1,70 @@
-import  {namesData}  from "./data/NamesData";
-import  { BabyNameEntry } from "./components/BabyNamesEntry";
-import { text } from "stream/consumers";
-import {useState} from "react";
-
-
-
-
-
-
+import { namesData } from "./data/NamesData";
+import { BabyNameEntry } from "./components/BabyNamesEntry";
+import { useState } from "react";
 
 function PageBody(): JSX.Element {
   const [searchInput, setSearchInput] = useState("");
+  const [favoriteNames, setFavoriteNames] = useState<BabyNameEntry[]>([]);
+  const [mainList, setMainList] = useState<BabyNameEntry[]>(namesData);
 
-  const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(event.target.value);
   };
 
-  const filteredEntries: BabyNameEntry[] = namesData.filter((names) =>
-    names.name.toLowerCase().includes(searchInput.toLowerCase())
-  );
-  
-
-    const sortedEntries: BabyNameEntry[] = filteredEntries
-      .slice()
-      .sort((a: BabyNameEntry, b: BabyNameEntry) => a.name.localeCompare(b.name));
-
-      const nameItems: JSX.Element[] = sortedEntries.map((entry) => (
-        
-        <p key={entry.id} className={entry.sex === "f" ? "girls" : "boys"}>
-          {entry.name}
-        </p>
-      ));
-    
-      return <div>
-              <input
-                    type="text"
-                    placeholder="Search here"
-                    onChange={handleChange}
-                    value={searchInput} 
-                    />
-                <hr />
-                <div className="all-names">{nameItems}</div>
-                <hr />
-             </div> 
+  const handleNameClick = (name: BabyNameEntry) => {
+    if (favoriteNames.includes(name)) {
+      setFavoriteNames((prevFavoriteNames) => prevFavoriteNames.filter((entry) => entry !== name));
+    } else {
+      setFavoriteNames((prevFavoriteNames) => [...prevFavoriteNames, name]);
     }
+  };
 
-    export default PageBody;
+  const handleFavoriteNameClick = (name: BabyNameEntry) => {
+    setFavoriteNames((prevFavoriteNames) => prevFavoriteNames.filter((entry) => entry !== name));
+  };
+
+  const filteredEntries: BabyNameEntry[] = mainList.filter((entry) =>
+    entry.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
+  const sortedEntries: BabyNameEntry[] = filteredEntries
+    .slice()
+    .sort((a: BabyNameEntry, b: BabyNameEntry) => a.name.localeCompare(b.name));
+
+  const favoriteNameItems: JSX.Element[] = favoriteNames.map((entry) => (
+    <p
+      key={entry.id}
+      className={`${entry.sex === "f" ? "girls" : "boys"} favorite-names`}
+      onClick={() => handleFavoriteNameClick(entry)}
+    >
+      {entry.name}
+    </p>
+  ));
+
+  const nameItems: JSX.Element[] = sortedEntries.map((entry) => (
+    <p
+      key={entry.id}
+      onClick={() => handleNameClick(entry)}
+      className={`${entry.sex === "f" ? "girls" : "boys"}`}
+    >
+      {entry.name}
+    </p>
+  ));
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      <hr />
+      <h2>Favorites: {favoriteNameItems}</h2>
+      <hr />
+      <div className="all-names">{nameItems}</div>
+    </div>
+  );
+}
+
+export default PageBody;
