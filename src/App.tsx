e76be469207c702214a2e12fbc1d29054a0df9ca
Laura Hall <podinjshmi1993@gmail.com>
2023-08-25 20:21:48 +0100

    removed unnecessary state and updated the apps name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import  PageBody  from "./PageBody";
+import PageBody from "./PageBody";
 import "./styles.css";
 
-
-
 function App(): JSX.Element {
-  return <div>
-            <h1>Hi babies</h1>
-            <PageBody />
-        </div>;
+  return (
+    <div>
+      <h1>Babies names app</h1>
+      <PageBody />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BabyNamesEntry.tsx
+++ src/components/BabyNamesEntry.tsx
@@ -1,27 +1,26 @@
-import { namesData } from '../data/NamesData';
+import { namesData } from "../data/NamesData";
 
 interface BabyNamesProps {
-    entries:typeof namesData;
+  entries: typeof namesData;
 }
 
 export interface BabyNameEntry {
-    id: number;
-    name: string;
-    sex : string;
+  id: number;
+  name: string;
+  sex: string;
 }
 
 const BabyNames = (props: BabyNamesProps): JSX.Element => {
-    const {entries} = props;
-    return (
-        <div>
-            {entries.map((entry, id) =>(
-            <section key={entry.id}>
-                <p>{entry.name}</p>
-            </section>
-
-            ))}
-        </div>
-    );
-}
+  const { entries } = props;
+  return (
+    <div>
+      {entries.map((entry) => (
+        <section key={entry.id}>
+          <p>{entry.name}</p>
+        </section>
+      ))}
+    </div>
+  );
+};
 
 export default BabyNames;
